96b30e98a63a48f1ecd1caee134174fd309286cf
Rinaldo23 <rinaldobadigar@gmail.com>
2023-02-08 16:30:39 +0530

    Login functionality

--- api/routes/authRouter.js
+++ api/routes/authRouter.js
@@ -1,8 +1,12 @@
 import express from "express"
-import { register } from "../controllers/authController.js";
+import { login, register } from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
+// REGISTER
 authRouter.post("/register", register);
+
+// LOGIN
+authRouter.post("/login", login);
 
 export default authRouter;
