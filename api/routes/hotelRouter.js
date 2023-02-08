9316a86e1f60d8793a8f21ee1e556cb27c9a93cc
Rinaldo23 <rinaldobadigar@gmail.com>
2023-02-08 13:55:19 +0530

    Restructing folder - MVC

--- api/routes/hotelRouter.js
+++ api/routes/hotelRouter.js
@@ -1,69 +1,22 @@
 import express from "express"
-import Hotel from "../models/hotelModel.js";
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotelController.js";
 
 const hotelRouter = express.Router();
 
 // CREATE HOTEL
-hotelRouter.post("/", async (req, res, next) => {
-
-    const newHotel = new Hotel(req.body);
-
-    try {
-        const savedHotel = await newHotel.save();
-        res.status(200).json(savedHotel);
-    } catch (error) {
-        next(err);
-    }
-
-})
+hotelRouter.post("/", createHotel)
 
 // UPDATE HOTEL
-hotelRouter.put("/:id", async (req, res, next) => {
-
-    try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
-        res.status(200).json(updatedHotel);
-    } catch (error) {
-        next(err);
-    }
-
-})
+hotelRouter.put("/:id", updateHotel)
 
 // DELETE HOTEL
-hotelRouter.delete("/:id", async (req, res, next) => {
-
-    try {
-        const deletedHotel = await Hotel.findByIdAndDelete(req.params.id);
-        res.status(200).json(deletedHotel);
-    } catch (error) {
-        next(err);
-    }
-
-})
+hotelRouter.delete("/:id", deleteHotel)
 
 // GET HOTEL
-hotelRouter.get("/:id", async (req, res, next) => {
-
-    try {
-        const hotel = await Hotel.findById(req.params.id);
-        res.status(200).json(hotel);
-    } catch (error) {
-        next(err);
-    }
-
-})
+hotelRouter.get("/:id", getHotel)
 
 // GET ALl HOTELS
-hotelRouter.get("/", async (req, res, next) => {
-
-    try {
-        const hotels = await Hotel.find();
-        res.status(200).json(hotels);
-    } catch (err) {
-        next(err);
-    }
-
-})
+hotelRouter.get("/", getHotels)
 
 
 export default hotelRouter;
