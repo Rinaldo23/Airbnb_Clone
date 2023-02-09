6b0dd4f7563ec32d215c87ddde64461b820f9580
Rinaldo23 <rinaldobadigar@gmail.com>
2023-02-09 14:13:32 +0530

    Update User - Logic

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,7 +1,6 @@
 import User from "../models/userModel.js";
 
 export const updateUser = async (req, res, next) => {
-
     try {
         const updatedUser = await User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
         res.status(200).json(updatedUser);
