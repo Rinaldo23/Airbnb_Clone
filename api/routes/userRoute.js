eeee620e3e5eb0728fbc92ef8390b4ab81a4ede4
Rinaldo23 <rinaldobadigar@gmail.com>
2023-02-09 14:02:08 +0530

    Verify User - Logic

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,14 +1,18 @@
 import express from "express"
 import { updateUser } from "../controllers/userController.js";
-import { verifyToken } from "../utils/verification.js";
+import { verifyToken, verifyUser } from "../utils/verification.js";
 
 const userRouter = express.Router();
 
 // UPDATE USER
-userRouter.put("/:id", updateUser)
+userRouter.put("/:id", updateUser);
 
-// userRouter.get("/checkauthentication", verifyToken, (req,res,next)=>{
-//   res.send("hello user, you are logged in")
-// })
+userRouter.get("/checkauthentication", verifyToken, (req,res,next)=>{
+  res.send("hello user, you are logged in")
+})
+
+userRouter.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+  res.send("hello user, you are logged in and you can delete your account")
+})
 
 export default userRouter;
