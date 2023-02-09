af8554d2efd24ff24e6a984747bfd5241e0d35e7
Rinaldo23 <rinaldobadigar@gmail.com>
2023-02-09 14:54:41 +0530

    Verification/Authorization for Hotels

--- api/routes/hotelRouter.js
+++ api/routes/hotelRouter.js
@@ -1,21 +1,23 @@
 import express from "express"
 import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotelController.js";
+import { verifyAdmin } from "../utils/verification.js";
 
 const hotelRouter = express.Router();
 
 // CREATE HOTEL
-hotelRouter.post("/", createHotel)
+hotelRouter.post("/", verifyAdmin, createHotel)
 
 // UPDATE HOTEL
-hotelRouter.put("/:id", updateHotel)
+hotelRouter.put("/:id", verifyAdmin, updateHotel)
 
 // DELETE HOTEL
-hotelRouter.delete("/:id", deleteHotel)
+hotelRouter.delete("/:id", verifyAdmin, deleteHotel)
 
 // GET HOTEL
 hotelRouter.get("/:id", getHotel)
 
 // GET ALl HOTELS
 hotelRouter.get("/", getHotels)
+
 
 export default hotelRouter;
