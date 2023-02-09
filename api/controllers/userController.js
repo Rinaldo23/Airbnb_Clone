3787f005abf68c4b6e10c2a790b4bc76845d3473
Rinaldo23 <rinaldobadigar@gmail.com>
2023-02-09 14:19:37 +0530

    Delete User - Logic

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -8,3 +8,13 @@ export const updateUser = async (req, res, next) => {
         next(err);
     }
 }
+
+export const deleteUser = async (req, res, next) => {
+
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+        res.status(200).json(deletedUser);
+    } catch (error) {
+        next(err);
+    }
+}
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,22 +1,25 @@
 import express from "express"
-import { updateUser } from "../controllers/userController.js";
-import { verifyAdmin, verifyToken, verifyUser } from "../utils/verification.js";
+import { deleteUser, updateUser } from "../controllers/userController.js";
+// import { verifyAdmin, verifyToken, verifyUser } from "../utils/verification.js";
 
 const userRouter = express.Router();
 
 // UPDATE USER
 userRouter.put("/:id", updateUser);
 
-userRouter.get("/checkauthentication", verifyToken, (req, res, next) => {
-    res.send("hello user, you are logged in")
-})
+// DELETE USER
+userRouter.delete("/:id", deleteUser);
 
-userRouter.get("/checkuser/:id", verifyUser, (req, res, next) => {
-    res.send("hello user, you are logged in and you can delete your account")
-})
+// userRouter.get("/checkauthentication", verifyToken, (req, res, next) => {
+//     res.send("hello user, you are logged in")
+// })
 
-userRouter.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
-    res.send("hello admin, you are logged in and you can delete all accounts")
-})
+// userRouter.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//     res.send("hello user, you are logged in and you can delete your account")
+// })
+
+// userRouter.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//     res.send("hello admin, you are logged in and you can delete all accounts")
+// })
 
 export default userRouter;
