21fba8bab617c79ce6d2942e37f3d0aa555317c9
Rinaldo23 <rinaldobadigar@gmail.com>
2023-02-11 17:18:16 +0530

    Header Section

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,77 @@
+import { faBed, faCalendarDays, faCar, faPerson, faPlane, faTaxi, } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Header.css";
+import { DateRange } from "react-date-range";
+import { useState } from "react";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { format } from "date-fns";
+
+const Header = ({ type }) => {
+    const [openDate, setOpenDate] = useState(false);
+    const [dates, setDates] = useState([
+        {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: "selection",
+        },
+    ]);
+    const [openOptions, setOpenOptions] = useState(false);
+    const [options, setOptions] = useState({
+        adult: 1,
+        children: 0,
+        room: 1,
+    });
+
+    const handleOption = (name, operation) => {
+        setOptions((prev) => {
+            return {
+                ...prev,
+                [name]: operation === "i" ? options[name] + 1 : options[name] - 1,
+            };
+        });
+    };
+
+    return (
+        <div className="header">
+            <div className={type === "list" ? "headerContainer listMode" : "headerContainer"}>
+                <div className="headerList">
+                    <div className="headerListItem active">
+                        <FontAwesomeIcon icon={faBed} />
+                        <span>Stays</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faPlane} />
+                        <span>Flights</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faCar} />
+                        <span>Car rentals</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faBed} />
+                        <span>Attractions</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faTaxi} />
+                        <span>Airport taxis</span>
+                    </div>
+                </div>
+                {type !== "list" && (
+                    <>
+                        <h1 className="headerTitle">
+                            A lifetime of discounts? It's Genius.
+                        </h1>
+                        <p className="headerDesc">
+                            Get rewarded for your travels – unlock instant savings of 10% or
+                            more with a free Lamabooking account
+                        </p>
+                        {/* {!user && <button className="headerBtn">Sign in / Register</button>} */}
+                    </>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Header;
