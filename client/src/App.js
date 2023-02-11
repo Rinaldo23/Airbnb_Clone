2f3a16d4888270b86f50075b19dd89b8ff1122e9
Rinaldo23 <rinaldobadigar@gmail.com>
2023-02-11 14:27:30 +0530

    Setting up Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home/Home";
+import Hotel from "./pages/Hotel/Hotel";
+import List from "./pages/List/List";
+import Login from "./pages/Login/Login";
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
