1d37c1cf4f1588f7afa2ea782a04063c50b54eb9
Rinaldo23 <rinaldobadigar@gmail.com>
2023-02-13 17:10:18 +0530

    Featured Properties

---
+++ client/src/components/FeaturedProperty/FeaturedProperties.jsx
@@ -0,0 +1,71 @@
+import "./FeaturedProperties.css"
+
+const FeaturedProperties = () => {
+    return (
+        <div>
+            <div className="fp">
+                <div className="fpItem" >
+                    <img
+                        src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                        alt=""
+                        className="fpImg"
+                    />
+                    <span className="fpName">Taj Hotel</span>
+                    <span className="fpCity">CST</span>
+                    <span className="fpPrice">Starting from $70</span>
+                    <div className="fpRating">
+                        <button>4⭐</button>
+                        <span>Excellent</span>
+                    </div>
+                </div>
+
+                <div className="fpItem" >
+                    <img
+                        src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                        alt=""
+                        className="fpImg"
+                    />
+                    <span className="fpName">Taj Hotel</span>
+                    <span className="fpCity">CST</span>
+                    <span className="fpPrice">Starting from $70</span>
+                    <div className="fpRating">
+                        <button>4⭐</button>
+                        <span>Excellent</span>
+                    </div>
+                </div>
+
+                <div className="fpItem" >
+                    <img
+                        src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                        alt=""
+                        className="fpImg"
+                    />
+                    <span className="fpName">Taj Hotel</span>
+                    <span className="fpCity">CST</span>
+                    <span className="fpPrice">Starting from $70</span>
+                    <div className="fpRating">
+                        <button>4⭐</button>
+                        <span>Excellent</span>
+                    </div>
+                </div>
+
+                <div className="fpItem" >
+                    <img
+                        src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                        alt=""
+                        className="fpImg"
+                    />
+                    <span className="fpName">Taj Hotel</span>
+                    <span className="fpCity">CST</span>
+                    <span className="fpPrice">Starting from $70</span>
+                    <div className="fpRating">
+                        <button>4⭐</button>
+                        <span>Excellent</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FeaturedProperties
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Featured from '../../components/Featured/Featured'
 import Header from '../../components/Header/Header'
 import Navbar from '../../components/Navbar/Navbar'
 import PropertyList from "../../components/PropertyList.jsx/PropertyList"
+import FeaturedProperties from "../../components/FeaturedProperty/FeaturedProperties"
 
 const Home = () => {
     return (
@@ -13,6 +14,8 @@ const Home = () => {
                 <Featured />
                 <h1 className="homeTitle">Browse by property type</h1>
                 <PropertyList />
+                <h1 className="homeTitle">Homes guests love</h1>
+                <FeaturedProperties />
             </div>
         </div>
     )
