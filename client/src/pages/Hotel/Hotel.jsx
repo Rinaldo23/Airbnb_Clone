8732a89199c007d053edf215cc26c444d0efafc7
Rinaldo23 <rinaldobadigar@gmail.com>
2023-02-14 16:29:26 +0530

    Hotel pagel UI

--- client/src/pages/Hotel/Hotel.jsx
+++ client/src/pages/Hotel/Hotel.jsx
@@ -1,8 +1,124 @@
-import React from 'react'
+import "./Hotel.css"
+import { faCircleArrowLeft, faCircleArrowRight, faCircleXmark, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react'
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import MailList from '../../components/MailList/MailList';
+import Navbar from '../../components/Navbar/Navbar';
 
 const Hotel = () => {
+
+  // const location = useLocation();
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [open, setOpen] = useState(false);
+
+  const photos = [
+    "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",
+    "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
+  ];
+
+  const handleOpen = (i) => {
+    setSlideNumber(i);
+    setOpen(true);
+  };
+
+  const handleMove = (direction) => {
+    let newSlideNumber;
+
+    if (direction === "l") {
+      newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1;
+    } else {
+      newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
+    }
+
+    setSlideNumber(newSlideNumber);
+  };
+
   return (
-    <div>Hotel</div>
+    <div>
+      <Navbar />
+      <Header type="list" />
+      <div className="hotelContainer">
+        {open && (
+          <div className="slider">
+            <FontAwesomeIcon
+              icon={faCircleXmark}
+              className="close"
+              onClick={() => setOpen(false)}
+            />
+            <FontAwesomeIcon
+              icon={faCircleArrowLeft}
+              className="arrow"
+              onClick={() => handleMove("l")}
+            />
+            <div className="sliderWrapper">
+              <img
+                src={photos[slideNumber]}
+                alt=""
+                className="sliderImg"
+              />
+            </div>
+            <FontAwesomeIcon
+              icon={faCircleArrowRight}
+              className="arrow"
+              onClick={() => handleMove("r")}
+            />
+          </div>
+        )}
+        <div className="hotelWrapper">
+          <button className="bookNow">Reserve or Book Now!</button>
+          <h1 className="hotelTitle">Oberoi</h1>
+          <div className="hotelAddress">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <span>Thane Mumbai 425555</span>
+          </div>
+          <span className="hotelDistance">
+            Excellent location – 500m from center
+          </span>
+          <span className="hotelPriceHighlight">
+            Book a stay over $250 at this property and get a
+            free airport taxi
+          </span>
+          <div className="hotelImages">
+            {photos?.map((photo, i) => (
+              <div className="hotelImgWrapper" key={i}>
+                <img
+                  onClick={() => handleOpen(i)}
+                  src={photo}
+                  alt=""
+                  className="hotelImg"
+                />
+              </div>
+            ))}
+          </div>
+          <div className="hotelDetails">
+            <div className="hotelDetailsTexts">
+              <h1 className="hotelTitle">Oberoi</h1>
+              <p className="hotelDesc">Thane Mumbai</p>
+            </div>
+            <div className="hotelDetailsPrice">
+              <h1>Perfect for a 10-night stay!</h1>
+              <span>
+                Located in the real heart of Krakow, this property has an
+                excellent location score of 9.8!
+              </span>
+              <h2>
+                <b>$2500</b> (10{" "}
+                nights)
+              </h2>
+              <button >Reserve or Book Now!</button>
+            </div>
+          </div>
+        </div>
+        <MailList />
+        <Footer />
+      </div>
+    </div>
   )
 }
 
